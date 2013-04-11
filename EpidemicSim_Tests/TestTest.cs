5284a4b7300e19f3c50973a72bf8e654b8b5ae6a
Florian Wilkens <floya@live.de>
2013-04-11 18:22:39 +0200

    Added xunit NuGet package, test project and some example tests

---
+++ EpidemicSim_Tests/TestTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace EpidemicSim_Tests
+{
+    public class ExampleTest
+    {
+        [Fact]
+        public void ExampleTestMethod()
+        {
+            // Simple asserts
+            Assert.Equal(4, 2 + 2);
+            Assert.True(42 < 1337);
+
+            // Testing wether a given Exception is thrown. The delegate just gets the Method to call.
+            Assert.Throws(typeof(IndexOutOfRangeException), new Assert.ThrowsDelegate(MethodThatThrowsOutOfRange));
+
+            // Testing wether no exception gets thrown. The delegate gets constructed through a delegate.
+            Assert.DoesNotThrow(new Assert.ThrowsDelegate(() => MethodWithoutException()));
+        }
+
+        private void MethodThatThrowsOutOfRange()
+        {
+            int[] i = new int[1];
+            int tmp = i[1];                   // Throws IndexOutOfRangeException
+        }
+
+        private void MethodWithoutException()
+        {
+            int[] i = new int[1];
+            int tmp = i[0];                   // Does not throw IndexOutOfRangeException
+        }
+    }
+}
