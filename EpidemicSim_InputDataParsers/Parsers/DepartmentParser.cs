794ca753aabe5e47bd175a0cdd118f4bfba66849
Dave J <contact_davej@ymail.com>
2013-04-14 16:13:55 +0200

    Pity, but the optimization seems not to work on every PC. So back to the roots.

--- EpidemicSim_InputDataParsers/Parsers/DepartmentParser.cs
+++ EpidemicSim_InputDataParsers/Parsers/DepartmentParser.cs
@@ -20,7 +20,7 @@ namespace PSC2013.ES.InputDataParsers.Parsers
         /// <returns>Fancy shit.</returns>
         public static Dictionary<string, List<Point>> Parse(string imageFile, List<Tuple<string, Point>> source)
         {
-            //Bitmap img = new Bitmap(imageFile);
+            Bitmap img = new Bitmap(imageFile);
 
             Dictionary<string, List<Point>> dict = new Dictionary<string,List<Point>>();
 
@@ -29,29 +29,26 @@ namespace PSC2013.ES.InputDataParsers.Parsers
             sw.Start();
 #endif
 
-            //foreach (Tuple<string, Point> tpl in source)
-            //{
-            //    #if DEBUG
-            //        Console.WriteLine(tpl.Item1);
-            //    #endif
+            foreach (Tuple<string, Point> tpl in source)
+            {
+                #if DEBUG
+                    Console.WriteLine(tpl.Item1);
+                #endif
 
-            //    List<Point> points = Fill(img, tpl.Item2);
-            //    if (dict.ContainsKey(tpl.Item1))
-            //        dict[tpl.Item1].AddRange(points);
-            //    else
-            //        dict[tpl.Item1] = points;
-            //} 00:01:53.55589394
+                TaskWorking(img, tpl, dict);
+            } // 00:02:12.7397335
 
-            Parallel.ForEach(source,
-                new ParallelOptions() { MaxDegreeOfParallelism = 3 },
-                item =>
-                {
-#if DEBUG
-                    Console.WriteLine(item.Item1);
-#endif
-                    Bitmap bmp = new Bitmap(imageFile);
-                    TaskWorking(bmp, item, dict);
-                });
+            // seems not to work on every pc. pity
+//            Parallel.ForEach(source,
+//                new ParallelOptions() { MaxDegreeOfParallelism = 3 },
+//                item =>
+//                {
+//#if DEBUG
+//                    Console.WriteLine(item.Item1);
+//#endif
+//                    Bitmap bmp = new Bitmap(imageFile);
+//                    TaskWorking(bmp, item, dict);
+//                });
             // 2 -- 00:01:23.8058131
             // 3 -- 00:01:10.0338862
             // 4 uses 1.5GB Memory => Out of Memory (btw. why?)
