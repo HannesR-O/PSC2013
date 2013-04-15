04e9936a668175cabd9a74be9636a8eb187e99ec
Florian Wilkens <floya@live.de>
2013-04-15 18:48:20 +0200

    Some Ideas and a first example of a Human class

--- EpidemicSim_Console/Program.cs
+++ EpidemicSim_Console/Program.cs
@@ -1,10 +1,17 @@
-﻿
+﻿using System;
+using PSC2013.ES.Library.Population;
+
 namespace PSC2013.ES.Console
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Human h = new Human();
+            var gH = h.GetGender();
+#if DEBUG
+            System.Console.ReadKey();
+#endif
         }
     }
 }
---
+++ EpidemicSim_Library/Population/Human.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSC2013.ES.Library.Population
+{
+    public class Human
+    {
+        /// <summary>
+        /// xxxx xxxx  xxxx xxxx  xxxx xxxx  xxxx xxxx
+        ///    4    8    12   16    20   24    28   32
+        /// 1. Byte: Gender (1bit) Age (2Bit) YearsTillNextAge (5Bit)
+        /// 2. Byte:
+        /// 3. Byte:
+        /// 4. Byte:
+        /// </summary>
+        private byte[] _data;
+
+        public Human()
+        {
+            _data = new byte[4];
+            new Random().NextBytes(_data);
+        }
+
+        public Gender GetGender()
+        {
+            return (Gender)(_data[0] & 128 >> 7);
+        }
+    }
+}
