db3f0969cb2a89eaf791e7bb9c79164e244e45ad
Dave J <contact_davej@ymail.com>
2013-04-18 11:11:55 +0200

    After crash now again... well. nobody saw it before. The initialization of PopulationCell is not how it was intended. It is not possible to init. a struct's field directly with definition...
    A parameterless constructor is also not possible. We should think about that again...

--- EpidemicSim_Library/AreaData/MatrixGenerator.cs
+++ EpidemicSim_Library/AreaData/MatrixGenerator.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,9 @@ namespace PSC2013.ES.Library.AreaData
 {
     public static class MatrixGenerator
     {
+        private const int WIDTH = 2814;
+        private const int HEIGHT = 3841;
+
         /// <summary>
         /// Generates
         /// </summary>
@@ -19,14 +23,35 @@ namespace PSC2013.ES.Library.AreaData
             PopulationCell[] populationArray,
             IEnumerable<DepartmentInfo> rawData)
         {
-            if (populationArray.Length != 10808574)
+            if (populationArray.Length != WIDTH * HEIGHT)
                 throw new ArgumentException(
                     "The argument has to be an array if PopulationCell with a length of '10808574'",
                     "populationArray");
 
-
+            foreach (DepartmentInfo item in rawData)
+                PopulateDepartment(item, populationArray);
 
             return populationArray;
+        }
+
+        private static void PopulateDepartment(DepartmentInfo depInfo, PopulationCell[] populationArray)
+        {
+            int minX = WIDTH;
+            int minY = HEIGHT;
+            int maxX = 0;
+            int maxY = 0;
+
+            foreach (Point item in depInfo.Coordinates)
+            {
+                minX = Math.Min(minX, item.X);
+                minY = Math.Min(minY, item.Y);
+                maxX = Math.Max(maxX, item.X);
+                maxY = Math.Max(maxY, item.Y);
+            }
+
+
+
+            // TODO | dj | continue..
         }
     }
 }
--- EpidemicSim_Library/PopulationData/PopulationCell.cs
+++ EpidemicSim_Library/PopulationData/PopulationCell.cs
@@ -8,13 +8,37 @@ namespace PSC2013.ES.Library.PopulationData
 {
     public struct PopulationCell
     {
+        private const int STANDARD_ARRAY_LENGTH = 8;
+
         public Human[] Humans;
         public int RefDepartment;
         public int HumanCount;
 
-        public void Init()
+        public void Init() // TODO | dj | should be called normally
         {
             Humans = new Human[10];
+        }
+
+        public void AddHuman(Human h)
+        {
+            for (int i = 0; i < Humans.Length; i++)
+            {
+                if (Humans[i].IsDead())
+                {
+                    Humans[i] = h;
+                    return;
+                }
+            }
+
+            int l = (Humans.Length <= 1) ? STANDARD_ARRAY_LENGTH : (int)(Humans.Length * 1.5f);
+            Human[] arr = new Human[l];
+
+            int n = 0;
+            foreach (Human human in Humans)
+            {
+                arr[n] = human;
+                n++;
+            }
         }
     }
 }
