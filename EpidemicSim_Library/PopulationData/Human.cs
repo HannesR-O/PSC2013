bfbef604293b1cc7be749a53229b2ec80900ad93
Florian Wilkens <floya@live.de>
2013-04-24 15:10:41 +0200

    Removed unecesary usings

--- EpidemicSim_Library/PopulationData/Human.cs
+++ EpidemicSim_Library/PopulationData/Human.cs
@@ -1,9 +1,5 @@
-﻿using PSC2013.ES.Library.Snapshot;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace PSC2013.ES.Library.PopulationData
 {
