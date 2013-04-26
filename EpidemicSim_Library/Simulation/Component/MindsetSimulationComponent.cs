541daea9aec16f2fe947536d4bc253964b75d223
HannesRO <1roebe@informatik.uni-hamburg.de>
2013-04-26 11:09:35 +0200

    Added MindsetSimulationComponent should be responsible for changing the Mindsets of every Human at every new day.

---
+++ EpidemicSim_Library/Simulation/Component/MindsetSimulationComponent.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSC2013.ES.Library.Simulation.Component
+{
+    public class MindsetSimulationComponent : ISimulationComponent
+    {
+        public void PerformSimulationStage(SimulationData data)
+        {
+           
+            //Update Mindsets every Day
+            //TODO |f| add Events to SimulationData to trigger events new Day/Month/Year? vs Using Simulators events?
+
+            if (data.CurrentHour == 0)
+            {
+
+                for (int i = 0; i < data.Population.Length; ++i)
+                {
+                    for (int j = 0; j < data.Population[i].Humans.Length; ++j)
+                    {
+
+                    }
+                }
+            }
+        }
+
+        public ESimulationStage GetSimulationStages()
+        {
+            return ESimulationStage.BeforeInfectedCalculation;
+        }
+    }
+}
