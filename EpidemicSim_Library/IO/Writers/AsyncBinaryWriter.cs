28fd8e2733a59cdae312d4651dbfbf1ae6b58447
Florian Wilkens <floya@live.de>
2013-04-27 12:52:24 +0200

    Cleaned up some ununsed IO classes and corrected some method comments

--- EpidemicSim_Library/IO/Writers/AsyncBinaryWriter.cs
+++ EpidemicSim_Library/IO/Writers/AsyncBinaryWriter.cs
@@ -1,9 +1,4 @@
 ﻿using PSC2013.ES.Library.IO.Files;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.IO;
 
 namespace PSC2013.ES.Library.IO.Writers
@@ -17,32 +12,10 @@ namespace PSC2013.ES.Library.IO.Writers
             if (File.Exists(fileName) && !overwrite)
                 return;
 
-            using (FileStream fs = new FileStream(fileName, FileMode.Create, FileAccess.Write))
+            using (var fs = new FileStream(fileName, FileMode.Create, FileAccess.Write))
             {
                 await fs.WriteAsync(bytes, 0, bytes.Length);
                 await fs.FlushAsync();
-            }
-        }
-
-        T IBinaryWriter.ReadFile<T>(string fileName)
-        {
-            using (FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read))
-            {
-                var header = fs.ReadByte();
-                byte[] bytes = new byte[fs.Length - 1];
-                fs.Read(bytes, 0, (int)(fs.Length - 1));        //TODO: |f| allows a max filesize of 2047 MB, might need to use a loop
-                //TODO: |f| not the best way to do this, because classes need to set their header correct // |t| I don#t see a problem here...
-                IBinaryFile file = null;
-                switch (header)
-                {
-                    case 0x1: // Simulation Info / Archive Header
-                        break;
-                    case 0x2: // Snapshot incl. CellsSnapshots
-                        break;
-                    default:
-                        throw new Exception("Unknown file header while reading " + fileName);
-                }
-                return (T)file;
             }
         }
     }
--- EpidemicSim_Library/IO/Writers/IBinaryWriter.cs
+++ EpidemicSim_Library/IO/Writers/IBinaryWriter.cs
@@ -1,28 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using PSC2013.ES.Library.IO.Files;
+﻿using PSC2013.ES.Library.IO.Files;
 
 namespace PSC2013.ES.Library.IO.Writers
 {
     public interface IBinaryWriter
     {
         /// <summary>
-        /// Writes the IDocument to the specified fileName
+        /// Writes a given IBinaryFile to the given path
         /// </summary>
-        /// <param name="file">The IDocument to write</param>
-        /// <param name="fileName">The fileName to write the document in</param>
+        /// <param name="file">The IBinaryFile to write</param>
+        /// <param name="filePath">The path to write the IBinaryFile to</param>
         /// <param name="overwrite">Flag to decide whether an existing file should be overwritten</param>        
-        void WriteFile(IBinaryFile file, string fileName, bool overwrite);
-
-        /// <summary>
-        /// Reads an IDocument from a given fileName
-        /// </summary>
-        /// <typeparam name="T">The Document to read</typeparam>
-        /// <param name="fileName"></param>
-        /// <returns></returns>
-        T ReadFile<T>(string fileName) where T : IBinaryFile;
+        void WriteFile(IBinaryFile file, string filePath, bool overwrite);
     }
 }
