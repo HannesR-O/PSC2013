b8767b9b2de1105f062b8abf85f129695c6728f8
1fechner <1fechner@informatik.uni-hamburg.de>
2013-05-02 21:47:07 +0200

    SimulationInfo FromFile init

--- EpidemicSim_Library/Snapshot/SimulationInfo.cs
+++ EpidemicSim_Library/Snapshot/SimulationInfo.cs
@@ -37,7 +37,56 @@ namespace PSC2013.ES.Library.Snapshot
         /// <returns>A new SimulationInfo</returns>
         public static SimulationInfo InitializeFromFile(byte[] bytes)
         {
-            throw new NotImplementedException();
+            Disease disease = new Disease();
+            disease.IncubationPeriod = BitConverter.ToInt32(bytes, 1);
+            disease.IdleTime = BitConverter.ToInt32(bytes, 5);
+            disease.SpreadingTime = BitConverter.ToInt32(bytes, 9);
+            disease.Transferability = BitConverter.ToInt32(bytes, 13);
+
+            int mortMaleBaby = BitConverter.ToInt32(bytes, 17);
+            int mortMaleChild = BitConverter.ToInt32(bytes, 21);
+            int mortMaleAdult = BitConverter.ToInt32(bytes, 25);
+            int mortMaleSenior = BitConverter.ToInt32(bytes, 29);
+            int mortFemaleBaby = BitConverter.ToInt32(bytes, 33);
+            int mortFemaleChild = BitConverter.ToInt32(bytes, 37);
+            int mortFemaleAdult = BitConverter.ToInt32(bytes, 41);
+            int mortFemaleSenior = BitConverter.ToInt32(bytes, 45);
+
+            disease.MortalityRate = 
+                new FactorContainer ( new[] {
+                mortMaleBaby, mortMaleChild, mortMaleAdult, mortMaleSenior, 
+                mortFemaleBaby, mortFemaleChild, mortFemaleAdult, mortFemaleSenior });
+
+            int healMaleBaby = BitConverter.ToInt32(bytes, 49);
+            int healMaleChild = BitConverter.ToInt32(bytes, 53);
+            int healMaleAdult = BitConverter.ToInt32(bytes, 57);
+            int healMaleSenior = BitConverter.ToInt32(bytes, 61);
+            int healFemaleBaby = BitConverter.ToInt32(bytes, 65);
+            int healFemaleChild = BitConverter.ToInt32(bytes, 69);
+            int healFemaleAdult = BitConverter.ToInt32(bytes, 73);
+            int healFemaleSenior = BitConverter.ToInt32(bytes, 77);
+
+            disease.HealingFactor =
+                new FactorContainer(new[] {
+                healMaleBaby, healMaleChild, healMaleAdult, healMaleSenior, 
+                healFemaleBaby, healFemaleChild, healFemaleAdult, healFemaleSenior });
+
+            int restMaleBaby = BitConverter.ToInt32(bytes, 81);
+            int restMaleChild = BitConverter.ToInt32(bytes, 85);
+            int restMaleAdult = BitConverter.ToInt32(bytes, 89);
+            int restMaleSenior = BitConverter.ToInt32(bytes, 93);
+            int restFemaleBaby = BitConverter.ToInt32(bytes, 97);
+            int restFemaleChild = BitConverter.ToInt32(bytes, 101);
+            int restFemaleAdult = BitConverter.ToInt32(bytes, 105);
+            int restFemaleSenior = BitConverter.ToInt32(bytes, 109);
+
+            disease.ResistanceFactor =
+                new FactorContainer(new[] {
+                restMaleBaby, restMaleChild, restMaleAdult, restMaleSenior, 
+                restFemaleBaby, restFemaleChild, restFemaleAdult, restFemaleSenior });
+
+            // TODO |t| Name, still to bound it...
+            return new SimulationInfo("test", disease);
         }
 
         /// <summary>
@@ -60,8 +109,8 @@ namespace PSC2013.ES.Library.Snapshot
             Array.Copy(BitConverter.GetBytes(Disease.MortalityRate.Female_Baby), 0, output, 33, 4);
             Array.Copy(BitConverter.GetBytes(Disease.MortalityRate.Female_Child), 0, output, 37, 4);
             Array.Copy(BitConverter.GetBytes(Disease.MortalityRate.Female_Adult), 0, output, 41, 4);
-            Array.Copy(BitConverter.GetBytes(Disease.MortalityRate.Female_Senior), 0, output, 47, 4);
-            Array.Copy(BitConverter.GetBytes(Disease.HealingFactor.Male_Baby), 0, output, 51, 4);
+            Array.Copy(BitConverter.GetBytes(Disease.MortalityRate.Female_Senior), 0, output, 45, 4);
+            Array.Copy(BitConverter.GetBytes(Disease.HealingFactor.Male_Baby), 0, output, 49, 4);
             Array.Copy(BitConverter.GetBytes(Disease.HealingFactor.Male_Child), 0, output, 53, 4);
             Array.Copy(BitConverter.GetBytes(Disease.HealingFactor.Male_Adult), 0, output, 57, 4);
             Array.Copy(BitConverter.GetBytes(Disease.HealingFactor.Male_Senior), 0, output, 61, 4);
@@ -75,7 +124,7 @@ namespace PSC2013.ES.Library.Snapshot
             Array.Copy(BitConverter.GetBytes(Disease.ResistanceFactor.Male_Senior), 0, output, 93, 4);
             Array.Copy(BitConverter.GetBytes(Disease.ResistanceFactor.Female_Baby), 0, output, 97, 4);
             Array.Copy(BitConverter.GetBytes(Disease.ResistanceFactor.Female_Child), 0, output, 101, 4);
-            Array.Copy(BitConverter.GetBytes(Disease.ResistanceFactor.Female_Adult), 0, output,105, 4);
+            Array.Copy(BitConverter.GetBytes(Disease.ResistanceFactor.Female_Adult), 0, output, 105, 4);
             Array.Copy(BitConverter.GetBytes(Disease.ResistanceFactor.Female_Senior), 0, output, 109, 4);
             Array.Copy(t, 0, output, 113, t.Length);
             return output;
