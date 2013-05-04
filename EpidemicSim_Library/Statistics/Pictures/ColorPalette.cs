893ac8f47ab3c815cd74d5df6e221d04791244b6
1fechner <1fechner@informatik.uni-hamburg.de>
2013-05-04 22:14:01 +0200

    Testing some Colors...

---
+++ EpidemicSim_Library/Statistics/Pictures/ColorPalette.cs
@@ -0,0 +1,13 @@
+﻿using System.Drawing;
+
+namespace PSC2013.ES.Library.Statistics.Pictures
+{
+    public class ColorPalette
+    {
+        static Color[] RED = {Color.FromArgb(255, 0, 0), 
+                                 Color.FromArgb(255, 50, 0), 
+                                 Color.FromArgb(255,75, 0), 
+                                 Color.FromArgb(255, 100, 0), 
+                                 Color.FromArgb(255, 125, 0)};
+    }
+}
