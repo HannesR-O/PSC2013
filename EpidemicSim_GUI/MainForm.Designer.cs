089834cf3ff88975e73ca1cabea3f10d7bf0c372
Dave J <contact_davej@ymail.com>
2013-05-05 21:16:56 +0200

    Gui-project added. start-window added.

---
+++ EpidemicSim_GUI/MainForm.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace PSC2013.ES.GUI
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MainMenuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.importToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.closeOpenedDataToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.createToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.createNewSimulationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.reviewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openSnapshotsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.MainSidePanel = new System.Windows.Forms.Panel();
+            this.MainPanel = new System.Windows.Forms.Panel();
+            this.MainPanel_pictureBox = new System.Windows.Forms.PictureBox();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.MainMenuStrip.SuspendLayout();
+            this.MainPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.MainPanel_pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MainMenuStrip
+            // 
+            this.MainMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.createToolStripMenuItem,
+            this.reviewToolStripMenuItem});
+            this.MainMenuStrip.Location = new System.Drawing.Point(0, 0);
+            this.MainMenuStrip.Name = "MainMenuStrip";
+            this.MainMenuStrip.Size = new System.Drawing.Size(484, 24);
+            this.MainMenuStrip.TabIndex = 0;
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openFileToolStripMenuItem,
+            this.importToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.closeOpenedDataToolStripMenuItem,
+            this.toolStripSeparator2,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openFileToolStripMenuItem
+            // 
+            this.openFileToolStripMenuItem.Name = "openFileToolStripMenuItem";
+            this.openFileToolStripMenuItem.Size = new System.Drawing.Size(217, 22);
+            this.openFileToolStripMenuItem.Text = "Open .dep...";
+            this.openFileToolStripMenuItem.Click += new System.EventHandler(this.openDepMap_Click);
+            // 
+            // importToolStripMenuItem
+            // 
+            this.importToolStripMenuItem.Name = "importToolStripMenuItem";
+            this.importToolStripMenuItem.Size = new System.Drawing.Size(217, 22);
+            this.importToolStripMenuItem.Text = "Open .sim...";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(214, 6);
+            // 
+            // closeOpenedDataToolStripMenuItem
+            // 
+            this.closeOpenedDataToolStripMenuItem.Name = "closeOpenedDataToolStripMenuItem";
+            this.closeOpenedDataToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.W)));
+            this.closeOpenedDataToolStripMenuItem.Size = new System.Drawing.Size(217, 22);
+            this.closeOpenedDataToolStripMenuItem.Text = "Close opened data";
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(214, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Q)));
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(217, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // createToolStripMenuItem
+            // 
+            this.createToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.createNewSimulationToolStripMenuItem});
+            this.createToolStripMenuItem.Name = "createToolStripMenuItem";
+            this.createToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.createToolStripMenuItem.Text = "Create";
+            // 
+            // createNewSimulationToolStripMenuItem
+            // 
+            this.createNewSimulationToolStripMenuItem.Name = "createNewSimulationToolStripMenuItem";
+            this.createNewSimulationToolStripMenuItem.Size = new System.Drawing.Size(201, 22);
+            this.createNewSimulationToolStripMenuItem.Text = "Create new simulation...";
+            this.createNewSimulationToolStripMenuItem.Click += new System.EventHandler(this.openDepMap_Click);
+            // 
+            // reviewToolStripMenuItem
+            // 
+            this.reviewToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openSnapshotsToolStripMenuItem});
+            this.reviewToolStripMenuItem.Name = "reviewToolStripMenuItem";
+            this.reviewToolStripMenuItem.Size = new System.Drawing.Size(56, 20);
+            this.reviewToolStripMenuItem.Text = "Review";
+            // 
+            // openSnapshotsToolStripMenuItem
+            // 
+            this.openSnapshotsToolStripMenuItem.Name = "openSnapshotsToolStripMenuItem";
+            this.openSnapshotsToolStripMenuItem.Size = new System.Drawing.Size(168, 22);
+            this.openSnapshotsToolStripMenuItem.Text = "Open snapshots...";
+            // 
+            // MainSidePanel
+            // 
+            this.MainSidePanel.Dock = System.Windows.Forms.DockStyle.Right;
+            this.MainSidePanel.Location = new System.Drawing.Point(284, 24);
+            this.MainSidePanel.Name = "MainSidePanel";
+            this.MainSidePanel.Size = new System.Drawing.Size(200, 438);
+            this.MainSidePanel.TabIndex = 1;
+            // 
+            // MainPanel
+            // 
+            this.MainPanel.Controls.Add(this.MainPanel_pictureBox);
+            this.MainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MainPanel.Location = new System.Drawing.Point(0, 24);
+            this.MainPanel.Name = "MainPanel";
+            this.MainPanel.Size = new System.Drawing.Size(284, 438);
+            this.MainPanel.TabIndex = 2;
+            // 
+            // MainPanel_pictureBox
+            // 
+            this.MainPanel_pictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MainPanel_pictureBox.Location = new System.Drawing.Point(0, 0);
+            this.MainPanel_pictureBox.Name = "MainPanel_pictureBox";
+            this.MainPanel_pictureBox.Size = new System.Drawing.Size(284, 438);
+            this.MainPanel_pictureBox.TabIndex = 0;
+            this.MainPanel_pictureBox.TabStop = false;
+            this.MainPanel_pictureBox.WaitOnLoad = true;
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.ReadOnlyChecked = true;
+            this.openFileDialog.RestoreDirectory = true;
+            this.openFileDialog.ShowReadOnly = true;
+            this.openFileDialog.SupportMultiDottedExtensions = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 462);
+            this.Controls.Add(this.MainPanel);
+            this.Controls.Add(this.MainSidePanel);
+            this.Controls.Add(this.MainMenuStrip);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Epidemic Simulator";
+            this.MainMenuStrip.ResumeLayout(false);
+            this.MainMenuStrip.PerformLayout();
+            this.MainPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.MainPanel_pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip MainMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem importToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem closeOpenedDataToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem createToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem createNewSimulationToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem reviewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openSnapshotsToolStripMenuItem;
+        private System.Windows.Forms.Panel MainSidePanel;
+        private System.Windows.Forms.Panel MainPanel;
+        private System.Windows.Forms.PictureBox MainPanel_pictureBox;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+    }
+}
+
---
+++ EpidemicSim_GUI/MainForm.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using PSC2013.ES.Library.IO.Readers;
+
+namespace PSC2013.ES.GUI
+{
+    public partial class MainForm : Form
+    {
+        private const string DEFAULT_DEP_EXTENSIONS = "Department-Files (*.dep)|*.dep|All files (*.*)|*.*";
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenDepMap()
+        {
+            openFileDialog.Filter = DEFAULT_DEP_EXTENSIONS;
+            openFileDialog.Title = "Select .dep-file.";
+            DialogResult dr = openFileDialog.ShowDialog();
+            if (dr == DialogResult.OK)
+            {
+                string filepath = openFileDialog.FileName;
+                DepartmentMapReader reader = new DepartmentMapReader(filepath);
+                Image img = reader.ReadImage();
+                // TODO | dj | now do something...
+                Console.WriteLine("done - " + img);
+            }
+        }
+
+        // == EVENTS ====== \\
+
+        private void openDepMap_Click(object sender, EventArgs e)
+        {
+            OpenDepMap();
+        }
+    }
+}
