b872fa4aeb2e4c60c438dd21a7da8ef66375bbf9
Dave J <contact_davej@ymail.com>
2013-05-13 17:12:02 +0200

    test commented! Re-build tests! Changes in Population-cell. should now be compilable...

--- EpidemicSim_Library/PopulationData/PopulationCell.cs
+++ EpidemicSim_Library/PopulationData/PopulationCell.cs
@@ -2,29 +2,76 @@
 
 namespace PSC2013.ES.Library.PopulationData
 {
-    public struct PopulationCell
+    public class PopulationCell
     {
         public int RefDepartment { get; set; }
 
-        public ushort Infected;
-        public ushort Diseased;
-        public ushort Spreading;
+        public ushort Infected { get; set; }
+        public ushort Diseased { get; set; }
+        public ushort Spreading { get; set; }
 
-        public ushort MaleBabies;
-        public ushort MaleChildren;
-        public ushort MaleAdults;
-        public ushort MaleSeniors;
-
-        public ushort FemaleBabies;
-        public ushort FemaleChildren;
-        public ushort FemaleAdults;
-        public ushort FemaleSeniors;
-
-        public int Probability;
-
-        internal void CalculateProbability()
+        public ushort MaleBabies
         {
-            throw new System.NotImplementedException();
+            get { return _data[0]; }
+            set { _data[0] = value; }
+        }
+        public ushort MaleChildren
+        {
+            get { return _data[1]; }
+            set { _data[1] = value; }
+        }
+        public ushort MaleAdults
+        {
+            get { return _data[2]; }
+            set { _data[2] = value; }
+        }
+        public ushort MaleSeniors
+        {
+            get { return _data[3]; }
+            set { _data[3] = value; }
+        }
+
+        public ushort FemaleBabies
+        {
+            get { return _data[4]; }
+            set { _data[4] = value; }
+        }
+        public ushort FemaleChildren
+        {
+            get { return _data[5]; }
+            set { _data[5] = value; }
+        }
+        public ushort FemaleAdults
+        {
+            get { return _data[6]; }
+            set { _data[6] = value; }
+        }
+        public ushort FemaleSeniors
+        {
+            get { return _data[7]; }
+            set { _data[7] = value; }
+        }
+
+        public ushort[] Data
+        {
+            get
+            {
+                ushort[] returnArray = new ushort[8];
+                _data.CopyToOtherArray(returnArray);
+                return returnArray;
+            }
+        }
+
+        /// <summary>
+        /// Probability to of a human meeting an infected human.
+        /// </summary>
+        public float Probability { get; set; }
+
+        private ushort[] _data;
+
+        public PopulationCell()
+        {
+            _data = new ushort[8];
         }
     }
 }
