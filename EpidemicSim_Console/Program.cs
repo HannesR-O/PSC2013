298c30fd320bf84520506310478372e93e0fdb79
1fechner <1fechner@informatik.uni-hamburg.de>
2013-06-11 18:43:30 +0200

    TestStats fixed

--- EpidemicSim_Console/Program.cs
+++ EpidemicSim_Console/Program.cs
@@ -257,6 +257,7 @@ namespace PSC2013.ES.Cmd
                 Console.WriteLine("Sum: {0}", sum);
 
                 //manager.CreateDeathGraphics(field, pal, prefix);
+                manager.CreateGraphics(field, pal, prefix);
                 Dictionary<string, Color> legend = manager.GetCaption();
 
                 foreach (string str in legend.Keys)
